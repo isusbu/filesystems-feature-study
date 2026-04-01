ebbf57213bb4512389021806fc7ca431f782f3d9
amirhnajafiz <najafizadeh21@gmail.com>
2026-04-01 15:50:12 -0400

    update: cleanup

--- lttng/cleanup.sh
+++ lttng/cleanup.sh
@@ -22,13 +22,24 @@ else
 fi
 
 # tracing parameters
+FS=ext4
 SUFFIX=$1
 SESSION_NAME="ext4-session-${SUFFIX}"
 OUTPUT_DIR="/mnt/gpfs/fs-study/${SESSION_NAME}"
 
+# read kernel probes for tracing from a target file
+KPROBE_FILE_PATH="filesystems/${FS}/kprobes.txt"
+if [ ! -f "$KPROBE_FILE_PATH" ]; then
+    echo "missing file: $KPROBE_FILE_PATH"
+    exit 1
+fi
+
 # export the tracing results
 babeltrace2 "/mnt/gpfs/fs-study/${SESSION_NAME}" > "/mnt/gpfs/fs-study/${SESSION_NAME}.out"
 # rm -rf "${OUTPUT_DIR}"
+
+# run the logparser
+lp -file "/mnt/gpfs/fs-study/${SESSION_NAME}.out" -init "${KPROBE_FILE_PATH}"
 
 # destroy the session
 lttng destroy "${SESSION_NAME}"
