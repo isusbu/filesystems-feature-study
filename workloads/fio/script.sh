b27403a52e3cd5c2667c1808ddb009b827a7172f
amirhnajafiz <najafizadeh21@gmail.com>
2026-04-08 11:23:06 -0400

    update: fio script

--- workloads/fio/script.sh
+++ workloads/fio/script.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-NAME="fio-workloads"
+NAME="amir-fio"
 
 # init LTTng session
-./lttng/init.sh "$NAME"
+# ./lttng/init.sh "$NAME"
 
 # list the fio jobs from jobs directory, and run them one by one
 for job in workloads/fio/jobs/*.fio; do
     echo "Running fio job: $job"
 
+    # clear the fio directory before each job to avoid interference from previous runs
+    rm -rf /mnt/ext4/*
+
     # start LTTng tracing for this job
     ./lttng/start.sh "$NAME"
 
     # run the fio job
-    sudo fio "$job" --directory=/mnt/ext4
+    sudo -g ext4_grp fio "$job" --directory=/mnt/ext4
 
     # stop LTTng tracing and save the trace with a name based on the job file name
     job_name=$(basename "$job" .fio)
